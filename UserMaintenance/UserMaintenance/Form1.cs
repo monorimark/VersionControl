d6161584f152b409eb938a2c7c75c9c1dd8916af
monorimark <mark.monori@stud.uni-corvinus.hu>
2022-09-30 15:01:06 +0200

    törlés megvalósítása

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -21,6 +21,7 @@ namespace UserMaintenance
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
             button2.Text = Resource1.Write;
+            button3.Text = Resource1.Delete;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -50,6 +51,17 @@ namespace UserMaintenance
                 sw.Close();
             }
             
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var chosen = (User)listBox1.SelectedItem;
+
+            var torlendo = (from x in users
+                            where chosen.ID == x.ID
+                            select x).FirstOrDefault();
+
+            users.Remove(torlendo);
         }
     }
 }
