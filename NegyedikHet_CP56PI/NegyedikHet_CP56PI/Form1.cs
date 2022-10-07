6b3dc56b138df2ad79e95f91849d55b613a6d3c7
monorimark <mark.monori@stud.uni-corvinus.hu>
2022-10-07 14:07:41 +0200

    7. CreateTable fv létrehozása, GetCell az excel koordinátákhoz

--- NegyedikHet_CP56PI/NegyedikHet_CP56PI/Form1.cs
+++ NegyedikHet_CP56PI/NegyedikHet_CP56PI/Form1.cs
@@ -45,7 +45,7 @@ namespace NegyedikHet_CP56PI
                 xlSheet = xlWB.ActiveSheet;
 
                 // Tábla létrehozása
-                //CreateTable(); // Ennek megírása a következő feladatrészben következik
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
 
                 // Control átadása a felhasználónak
                 xlApp.Visible = true;
@@ -63,6 +63,64 @@ namespace NegyedikHet_CP56PI
                 xlWB = null;
                 xlApp = null;
             }
+        }
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+                "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+
+            object[,] values = new object[flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator == true){
+                    values[counter, 4] = "Van";
+                }
+                else{
+                    values[counter, 4] = "Nincs";
+                }
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }
