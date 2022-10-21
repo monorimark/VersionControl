df24cda9cd1a0c6e899c3c3cf4d096e569f480d8
monorimark <mark.monori@stud.uni-corvinus.hu>
2022-10-21 13:44:03 +0200

    3. Webszolgáltatás behívása

--- Otodikhet_CP56PI/Otodikhet_CP56PI/Form1.cs
+++ Otodikhet_CP56PI/Otodikhet_CP56PI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Otodikhet_CP56PI.MbnServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,19 @@ namespace Otodikhet_CP56PI
         public Form1()
         {
             InitializeComponent();
+            WebHivas();
+        }
+        private void WebHivas()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
