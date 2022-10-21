f886c56f440b99e65eafc3cbf4dc8fa57b4b9b4d
monorimark <mark.monori@stud.uni-corvinus.hu>
2022-10-21 14:27:37 +0200

    7. Dinamikus finkciók megvalósítása

--- Otodikhet_CP56PI/Otodikhet_CP56PI/Form1.cs
+++ Otodikhet_CP56PI/Otodikhet_CP56PI/Form1.cs
@@ -19,19 +19,25 @@ namespace Otodikhet_CP56PI
         public Form1()
         {
             InitializeComponent();
-            //WebHivas();
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
             dataGridView1.DataSource = Rates;
             XMLFeldolg(WebHivas());
             adatMegjelenit();
         }
+
         private string WebHivas()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = (comboBox1.SelectedItem).ToString(),
+                startDate = (dateTimePicker1.Value).ToString(),
+                endDate = (dateTimePicker2.Value).ToString()
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
@@ -68,6 +74,7 @@ namespace Otodikhet_CP56PI
             series.ChartType = SeriesChartType.Line;
             series.XValueMember = "Date";
             series.YValueMembers = "Value";
+            series.BorderWidth = 2;
 
             var legend = chartRateData.Legends[0];
             legend.Enabled = false;
@@ -76,6 +83,21 @@ namespace Otodikhet_CP56PI
             chartArea.AxisX.MajorGrid.Enabled = false;
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
