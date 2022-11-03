ef36ddbfc70555988c52244509c12024474a63cf
monorimark <mark.monori@stud.uni-corvinus.hu>
2022-11-03 13:57:47 +0100

    gyártás hiba javítás

--- HatodikHet_CP56PI/HatodikHet_CP56PI/Entitites/Ball.cs
+++ HatodikHet_CP56PI/HatodikHet_CP56PI/Entitites/Ball.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace HatodikHet_CP56PI.Entitites
 {
-    class Ball : Label
+    public class Ball : Label
     {
         public Ball()
         {
--- HatodikHet_CP56PI/HatodikHet_CP56PI/Entitites/BallFactory.cs
+++ HatodikHet_CP56PI/HatodikHet_CP56PI/Entitites/BallFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HatodikHet_CP56PI.Entitites
 {
-    class BallFactory
+    public class BallFactory
     {
         public Ball CreateNew()
         {
