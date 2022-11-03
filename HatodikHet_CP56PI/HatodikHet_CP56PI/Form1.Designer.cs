0c5276e1cbbdffa9ad51385760a205ba4c5ddac6
monorimark <mark.monori@stud.uni-corvinus.hu>
2022-11-03 15:10:32 +0100

    labda átszínezése

--- HatodikHet_CP56PI/HatodikHet_CP56PI/Form1.Designer.cs
+++ HatodikHet_CP56PI/HatodikHet_CP56PI/Form1.Designer.cs
@@ -95,11 +95,12 @@ namespace HatodikHet_CP56PI
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.DarkRed;
             this.button3.Location = new System.Drawing.Point(95, 42);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
--- HatodikHet_CP56PI/HatodikHet_CP56PI/Form1.cs
+++ HatodikHet_CP56PI/HatodikHet_CP56PI/Form1.cs
@@ -66,7 +66,10 @@ namespace HatodikHet_CP56PI
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory
+            {
+                BallColor = button3.BackColor
+            };
         }
         private void DisplayNext()
         {
