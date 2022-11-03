0b3231dd85bb28ccc6ecd43a021494203bfc522c
monorimark <mark.monori@stud.uni-corvinus.hu>
2022-11-03 14:12:33 +0100

    autó betöltése, kirajzoltatása

---
+++ HatodikHet_CP56PI/HatodikHet_CP56PI/Entitites/Car.cs
@@ -0,0 +1,19 @@
+﻿using HatodikHet_CP56PI.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatodikHet_CP56PI.Entitites
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imgFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imgFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
