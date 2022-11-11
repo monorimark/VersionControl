8fdf0956984e2b4becb50c0955ba7ebb2b0ff551
monorimark <mark.monori@stud.uni-corvinus.hu>
2022-11-11 13:48:17 +0100

    Adatok betöltése

--- HetedikHet_CP56PI/HetedikHet_CP56PI/Form1.cs
+++ HetedikHet_CP56PI/HetedikHet_CP56PI/Form1.cs
@@ -12,9 +12,13 @@ namespace HetedikHet_CP56PI
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
