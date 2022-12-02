9e9cc87166e3935b35b01a0d066e7224f800f408
monorimark <mark.monori@stud.uni-corvinus.hu>
2022-12-02 14:07:00 +0100

    adatok betöltése, 3 fájl

--- kilencedikhet/kilencedikhet/Form1.cs
+++ kilencedikhet/kilencedikhet/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using kilencedikhet.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace kilencedikhet
 {
@@ -15,6 +17,76 @@ namespace kilencedikhet
         public Form1()
         {
             InitializeComponent();
+            Population = PLoad(@"C:\Temp\nép.csv");
+            BirthProbabilities = BPLoad(@"C:\Temp\születés.csv");
+            DeathProbabilities = DPLoad(@"C:\Temp\halál.csv");
+
+            //dataGridView1.DataSource = Population;
+        }
+
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
+        public List<Person> PLoad(string file)
+        {
+            List<Person> population = new List<Person>();
+
+            StreamReader f = new StreamReader(file, Encoding.Default);
+            while (!f.EndOfStream)
+            {
+                var sor = f.ReadLine().Split(';');
+                Person p = new Person
+                {
+                    BirthYear = int.Parse(sor[0]),
+                    Gender = (Gender)Enum.Parse(typeof(Gender), sor[1]),
+                    NbrOfChildren = int.Parse(sor[2])
+                };
+                population.Add(p);
+            }
+
+            f.Close();
+            return population;
+        }
+        public List<BirthProbability> BPLoad(string file)
+        {
+            List<BirthProbability> birthProbabilities = new List<BirthProbability>();
+
+            StreamReader f = new StreamReader(file, Encoding.Default);
+            while (!f.EndOfStream)
+            {
+                var sor = f.ReadLine().Split(';');
+                BirthProbability bp = new BirthProbability
+                {
+                    Age = int.Parse(sor[0]),
+                    NbrOfChildren = int.Parse(sor[1]),
+                    P = double.Parse(sor[2])
+                };
+                birthProbabilities.Add(bp);
+            }
+
+            f.Close();
+            return birthProbabilities;
+        }
+        public List<DeathProbability> DPLoad(string file)
+        {
+            List<DeathProbability> deathProbabilities = new List<DeathProbability>();
+
+            StreamReader f = new StreamReader(file, Encoding.Default);
+            while (!f.EndOfStream)
+            {
+                var sor = f.ReadLine().Split(';');
+                DeathProbability p = new DeathProbability
+                {
+                    Gender = (Gender)Enum.Parse(typeof(Gender), sor[0]),
+                    Age = int.Parse(sor[1]),
+                    P = double.Parse(sor[2])
+                };
+                deathProbabilities.Add(p);
+            }
+
+            f.Close();
+            return deathProbabilities;
         }
     }
 }
