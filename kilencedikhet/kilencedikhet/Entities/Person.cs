17d69c17f125a550d7365e3076643ebd48f2232c
monorimark <mark.monori@stud.uni-corvinus.hu>
2022-12-02 13:49:36 +0100

    classok létrehozása

---
+++ kilencedikhet/kilencedikhet/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kilencedikhet.Entities
+{
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsALive { get; set; }
+
+        public Person()
+        {
+            IsALive = true;
+        }
+    }
+}
