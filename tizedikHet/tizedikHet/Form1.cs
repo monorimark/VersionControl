2c6f72f6a277ef2cbbc3becf92adff8268df6188
monorimark <mark.monori@stud.uni-corvinus.hu>
2022-12-09 14:05:17 +0100

    iterációs lépés felépítése

--- tizedikHet/tizedikHet/Form1.cs
+++ tizedikHet/tizedikHet/Form1.cs
@@ -34,7 +34,19 @@ namespace tizedikHet
             {
                 gc.AddPlayer(nbrOfSteps);
             }
+            gc.GameOver += Gc_GameOver;
             gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format("{0}. generáció", generation);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
         }
     }
 }
